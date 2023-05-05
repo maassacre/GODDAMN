174ff50c6ec86600951a92ff15be70906a18da87
maassacre <116222003+maassacre@users.noreply.github.com>
2023-05-05 20:56:45 +0500

    Update pts.js

--- pts.js
+++ pts.js
@@ -17,15 +17,25 @@ function calc() {
     // alert (typeof(Number(lastpts.innerHTML)))
     let numlastpts = Number(lastpts.innerHTML)
     let numrespts = Number(currentpts.innerHTML)
+    // if (!isNaN(currentpts)){
+    
+    if (currentpts !== 0 && !isNaN(currentpts) && currentpts >= 10 && (currentpts ^ 0) === currentpts ){
 
-    if (numlastpts > currentpts) {
-        respts.style.color = '#FF0000'
-        respts.innerHTML = `- ${numlastpts - currentpts}`
+        if (numlastpts > currentpts) {
+        
+            respts.style.color = '#FF0000';
+            // isNaN(currentpts) ? respts.innerHTML = "введите свои эмэмэры" :
+            respts.innerHTML =  `-${numlastpts - currentpts}`;
+        }
+        else {
+            respts.style.color = '#00FF00';
+            // isNaN(currentpts) ? respts.innerHTML = "введите свои эмэмэры" :
+            respts.innerHTML = `+${currentpts - numlastpts}`;
+            
+        }
+        lastpts.innerHTML = currentpts
     }
-    else {
-        respts.style.color = '#00FF00'
-        respts.innerHTML = `+ ${currentpts - numlastpts}`
-    }
-    lastpts.innerHTML = currentpts
-    // respts.innerHTML = currentpts;
+    else{
+        respts.style.color = '#999999';
+        respts.innerHTML = "введите свои эмэмэры"}
 }
